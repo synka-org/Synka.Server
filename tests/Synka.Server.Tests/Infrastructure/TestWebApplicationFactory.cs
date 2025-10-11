636a1acfa3839a54ae9d637e4cf99481b5d738bb
Olov Nilzen <olov@nilzen.se>
2025-10-11 22:52:34 +0200

    style: fix file encoding in TestWebApplicationFactory

--- tests/Synka.Server.Tests/Infrastructure/TestWebApplicationFactory.cs
+++ tests/Synka.Server.Tests/Infrastructure/TestWebApplicationFactory.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
